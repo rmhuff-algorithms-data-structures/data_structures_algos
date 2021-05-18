15e498a32277653835f8c897d61f95ca25070780
Ryan M Hufford <ryanmhufford@Ryans-MacBook-Pro.local>
2021-05-18 15:14:28 -0400

    added zipped files, started notes creating algos

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] names) {
+        int[] arr = {10,20,30};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr.length);
+    }
+}
+
